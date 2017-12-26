3de97ac73f9a09931c2e8ca8f01301cb0f158cdd
stellacanessa <32998854+stellacanessa@users.noreply.github.com>
2017-12-26 17:28:46 +0100

    Update PlayingCard.java

--- PlayingCard.java
+++ PlayingCard.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package interfaceex3;
 
 // PlayingCard.java
-// Author: Rob Miller
-// Program last changed: 27 October 2008
+// Author: Rob Miller; adapted and expanded by Stella Canessa
+// Program last changed: 26 November 2017
     
 public class PlayingCard implements Comparable <PlayingCard>
 {
